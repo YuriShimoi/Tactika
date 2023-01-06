3b4404de4ffba70b0e6229d194705c3ee9e2ceb8
yurishimoi <shimoi.yuri@gmail.com>
2023-01-05 21:38:26 -0300

    some shaders done or semi

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let MAPPING = [];
 
 for(let _y=0; _y<20; _y++) {
     for(let _x=0; _x<5; _x++) {
-        MAPPING.push(new HexTile(_x, _y, Math.round(Math.random()+1)));
+        MAPPING.push(new HexTile(_x, _y, Math.round(Math.random()*2+1)));
     }
 }
 
