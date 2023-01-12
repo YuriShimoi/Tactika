c4b88d59f237454bccefccee37f831360a6be11f
Yuri Shimoi <shimoi.yuri@gmail.com>
2023-01-12 15:43:49 -0300

    visual tinkering

--- main.js
+++ main.js
@@ -14,8 +14,34 @@ for(let _y=0; _y<20; _y++) {
     }
 }
 
+function adjustSizes() {
+    let _main   = document.getElementById("main");
+    let _lowest = {'top': null, 'right': null, 'bottom':null, 'left':null};
+    for(let _tile of _main.getElementsByClassName("hxt-tile")) {
+        let _viewportOffset = _tile.getBoundingClientRect();
+
+        if(_lowest.top === null || _viewportOffset.top < _lowest.top)
+            _lowest.top = _viewportOffset.top;
+
+        if(_lowest.right === null || (document.body.offsetWidth - _viewportOffset.right) < _lowest.right)
+            _lowest.right = (document.body.offsetWidth - _viewportOffset.right);
+
+        if(_lowest.bottom === null || (document.body.offsetHeight - _viewportOffset.bottom) < _lowest.bottom)
+            _lowest.bottom = (document.body.offsetHeight - _viewportOffset.bottom);
+
+        if(_lowest.left === null || _viewportOffset.left < _lowest.left)
+            _lowest.left = _viewportOffset.left;
+    }
+    console.log(_lowest);
+    let _width  = document.body.offsetWidth  - (_lowest.left + _lowest.right);
+    let _height = document.body.offsetHeight - (_lowest.top  + _lowest.bottom);
+    _main.style.width  = `${_width}px`;
+    _main.style.height = `${_height}px`;
+}
+
 function start() {
     HexTiler.draw(ELEMENT, MAPPING);
+    adjustSizes();
 }
 
 start();
